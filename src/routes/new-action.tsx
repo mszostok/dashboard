e3faf221dc1357daa6044f113698645cc825643d
Mateusz Szostok <szostok.mateusz@gmail.com>
2021-12-23 16:35:59 +0100

    Add catalog, change page layout, ask about input only if needed

--- src/routes/new-action.tsx
+++ src/routes/new-action.tsx
@@ -1,10 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Breadcrumb } from "antd";
 import Page from "../layout/Page";
 import WizardContainer from "../wizard/Wizard.container";
 
 function NewAction() {
+  const { path, revision } = useParams();
+
+  if (!path) {
+    return <p>Interface path for Action cannot be empty</p>;
+  }
+  if (!revision) {
+    return <p>Interface revision for Action cannot be empty</p>;
+  }
+
   const breadcrumb = (
     <Breadcrumb>
       <Breadcrumb.Item>
@@ -15,8 +24,12 @@ function NewAction() {
   );
 
   return (
-    <Page breadcrumb={breadcrumb} title="Create a new Action">
-      <WizardContainer />
+    <Page
+      breadcrumb={breadcrumb}
+      title="Create a new Action"
+      onBack={() => window.history.back()}
+    >
+      <WizardContainer interfacePath={path} interfaceRevision={revision} />
     </Page>
   );
 }
